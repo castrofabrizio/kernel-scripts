310205c3462a641457e46030c9a62562da7c3257
Fabrizio Castro <fabrizio.castro@gmx.co.uk>
2019-09-27 10:03:34 +0100

    Improvements to the selection of the build directory
    Signed-off-by: Fabrizio Castro <fabrizio.castro@gmx.co.uk>

--- select-machine.sh
+++ select-machine.sh
@@ -1,24 +1,55 @@
 #!/bin/bash
-# This is to select a machine to work with
+# This script is to select a machine to work with, and has to be sourced.
+# It requires a parameter: the machine name. If run without parameters, it'll
+# print the available machines
 
 PROGRAM_DIRECTORY="$(dirname "$(realpath "${BASH_SOURCE[0]}")")"
 if [ -z "${UTILS_LOADED}" ]; then
 	source "${PROGRAM_DIRECTORY}/utils.sh"
 fi
-export MACHINE=$1
-export BUILD_DIRECTORY="${BUILD_DIRECTORY:-${PWD}/build-${MACHINE}}"
+export MACHINE="$1"
 
+# Let's take care of the build directory
+DEFAULT_BUILD_DIRECTORY="${PWD}/build-${MACHINE}"
+if [ -z "${BUILD_DIRECTORY+x}" ]; then
+	BUILD_DIRECTORY="${DEFAULT_BUILD_DIRECTORY}"
+fi
+export BUILD_DIRECTORY
+
+# Have we been sourced?
 if [[ "${BASH_SOURCE[0]}" == "${0}" ]]; then
 	echo "Please, source me" | print_error
 	exit 1
 fi
 
 set +e
+# Is the configuration correct?
 if ! ${PROGRAM_DIRECTORY}/common.sh; then
 	unset MACHINE
 	return 1
 fi
 set -e
 
+# Time to change the shell prompt
 . "${PROGRAM_DIRECTORY}/select-prompt.sh"
-echo "Machine \"${MACHINE}\" configured" | print_info
+
+# Confirm the parameters with the user
+cat<<EOF | print_info
+
+#################
+# Configuration #
+#################
+
+Machine: "${MACHINE}"
+Selected build directory: "${BUILD_DIRECTORY}"
+
+EOF
+
+if [ "${BUILD_DIRECTORY}" != "${DEFAULT_BUILD_DIRECTORY}" ]; then
+	cat<<-EOF | print_info
+	(the default build directory is: "${DEFAULT_BUILD_DIRECTORY}", if you rather
+	use the default build directory unset BUILD_DIRECTORY and then select the
+	machine again)
+
+	EOF
+fi
