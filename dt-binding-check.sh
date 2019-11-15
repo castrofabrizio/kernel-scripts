bfeeeb04321a4ca05a609bc6b6fa71311c2ce736
Fabrizio Castro <fabrizio.castro@gmx.co.uk>
2019-11-15 15:04:50 +0000

    Allow for DT_SCHEMA_FILES
    Signed-off-by: Fabrizio Castro <fabrizio.castro@gmx.co.uk>

--- dt-binding-check.sh
+++ dt-binding-check.sh
@@ -5,11 +5,16 @@ PROGRAM_DIRECTORY="$(cd "$(dirname "$0")"; pwd; )"
 source "${PROGRAM_DIRECTORY}/common.sh"
 source "${ENVIRONMENT_FILE}"
 
+COMMAND="dt_binding_check"
+if [ $# -ne 0 ]; then
+	COMMAND="${COMMAND} DT_SCHEMA_FILES=${1}"
+fi
+
 ${PROGRAM_DIRECTORY}/compile-linux.sh \
 	-k \
 	-t ${ENVIRONMENT_FILE} \
 	-b "${BUILD_DIRECTORY}" \
-	-K "dt_binding_check" \
+	-K "${COMMAND}" \
 	-p \
 	| print_no_label
 check_exit_value ${PIPESTATUS[0]}
