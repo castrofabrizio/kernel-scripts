65ed32eb567e523be2fc2e20ba3afd3458571899
Fabrizio Castro <fabrizio.castro@gmx.co.uk>
2019-11-15 15:15:02 +0000

    Add dtbs.sh
    Signed-off-by: Fabrizio Castro <fabrizio.castro@gmx.co.uk>

--- dtbs.sh
+++ dtbs.sh
@@ -1,17 +1,23 @@
 #!/bin/bash
-# This script helps with the compilation of specific device tree files
+# This script helps with the compilation of device tree files
 
 PROGRAM_DIRECTORY="$(cd "$(dirname "$0")"; pwd; )"
 source "${PROGRAM_DIRECTORY}/common.sh"
 source "${ENVIRONMENT_FILE}"
 
+echo "Compiling device trees" | print_info
+"${PROGRAM_DIRECTORY}"/compile-linux.sh \
+	-k \
+	-t "${ENVIRONMENT_FILE}" \
+	-b "${BUILD_DIRECTORY}" \
+	-K "dtbs W=1" | \
+	print_no_label
+check_exit_value ${PIPESTATUS[0]}
+
 for CURRENT_DEVICE_TREE_BINARY in ${DEVICE_TREE_FILES}; do
 	EXTENSION="${CURRENT_DEVICE_TREE_BINARY##*.}"
 	FILENAME="${CURRENT_DEVICE_TREE_BINARY%.*}"
 	CURRENT_DEVICE_TREE_SOURCE="${FILENAME}.dts"
-	rm -f \
-		"${BUILD_DIRECTORY}/arch/${ARCH}/boot/dts/${CURRENT_DEVICE_TREE_SOURCE}" \
-		"${BUILD_DIRECTORY}/arch/${ARCH}/boot/dts/${CURRENT_DEVICE_TREE_BINARY}"
 	if [ "${EXTENSION}" != "dtb" ]; then
 		cat<<-EOF | print_warning
 		File:
@@ -24,15 +30,6 @@ for CURRENT_DEVICE_TREE_BINARY in ${DEVICE_TREE_FILES}; do
 		echo "\"arch/${ARCH}/boot/dts/${CURRENT_DEVICE_TREE_SOURCE}\" No such file. Skipping." | print_warning
 		continue
 	fi
-	echo "Compiling \"${CURRENT_DEVICE_TREE_BINARY}\"" | print_info
-	${PROGRAM_DIRECTORY}/compile-linux.sh \
-		-k \
-		-t ${ENVIRONMENT_FILE} \
-		-b "${BUILD_DIRECTORY}" \
-		-K "${CURRENT_DEVICE_TREE_BINARY} W=1" | \
-		print_no_label
-	check_exit_value ${PIPESTATUS[0]}
-
 	echo "Reverse engineering ${CURRENT_DEVICE_TREE_BINARY}" | print_info
 	"${BUILD_DIRECTORY}/scripts/dtc/dtc" \
 		-I dtb \
