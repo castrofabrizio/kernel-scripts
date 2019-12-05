be9b75e21001032f5b0afd10ee63a92d7f270682
Fabrizio Castro <fabrizio.castro@gmx.co.uk>
2019-12-05 15:52:55 +0000

    Allow for non *Image kernel images
    Signed-off-by: Fabrizio Castro <fabrizio.castro@gmx.co.uk>

--- kernel-deploy.sh
+++ kernel-deploy.sh
@@ -5,24 +5,24 @@ PROGRAM_DIRECTORY="$(cd "$(dirname "$0")"; pwd; )"
 source "${PROGRAM_DIRECTORY}/common.sh"
 source "${ENVIRONMENT_FILE}"
 
-rm -f ${BUILD_DIRECTORY}/arch/${ARCH}/boot/${KERNEL_IMAGE}.gz
+case ${KERNEL_IMAGE} in
+	*Image)
+		IMAGE_FILEPATH="${BUILD_DIRECTORY}/arch/${ARCH}/boot/${KERNEL_IMAGE}"
+		;;
+	*)
+		IMAGE_FILEPATH="${BUILD_DIRECTORY}/${KERNEL_IMAGE}"
+		;;
+esac
+
+rm -f "${IMAGE_FILEPATH}.gz"
 
 echo "Compressing ${KERNEL_IMAGE}..." | print_info
-gzip \
-	-9 \
-	-c ${BUILD_DIRECTORY}/arch/${ARCH}/boot/${KERNEL_IMAGE} \
-	> ${BUILD_DIRECTORY}/arch/${ARCH}/boot/${KERNEL_IMAGE}.gz
+gzip -9 -c ${IMAGE_FILEPATH} > ${IMAGE_FILEPATH}.gz
 
 for KERNEL_DEPLOY_DIRECTORY in ${KERNEL_DEPLOY_DIRECTORIES}; do
 	echo "Deploying ${KERNEL_IMAGE} to \"${KERNEL_DEPLOY_DIRECTORY}\"..." | print_info
-	cp \
-		--remove-destination \
-		${BUILD_DIRECTORY}/arch/${ARCH}/boot/${KERNEL_IMAGE} \
-		"${KERNEL_DEPLOY_DIRECTORY}"
+	cp --remove-destination ${IMAGE_FILEPATH} "${KERNEL_DEPLOY_DIRECTORY}"
 	echo "Deploying ${KERNEL_IMAGE}.gz to \"${KERNEL_DEPLOY_DIRECTORY}\"..." | print_info
-	cp \
-		--remove-destination \
-		${BUILD_DIRECTORY}/arch/${ARCH}/boot/${KERNEL_IMAGE}.gz \
-		"${KERNEL_DEPLOY_DIRECTORY}"
+	cp --remove-destination ${IMAGE_FILEPATH}.gz "${KERNEL_DEPLOY_DIRECTORY}"
 done
 echo "All done" | print_info
