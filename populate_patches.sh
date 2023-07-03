cc039526bc58c4a81b5f781553d53d47ef964795
Fabrizio Castro <fabrizio.castro.jz@renesas.com>
2023-07-03 17:59:54 +0100

    [HACK] Improvements
    Signed-off-by: Fabrizio Castro <fabrizio.castro.jz@renesas.com>

--- populate_patches.sh
+++ populate_patches.sh
@@ -1,12 +1,35 @@
 #!/bin/bash -e
 PROGRAM_DIRECTORY="$(cd "$(dirname "$0")"; pwd; )"
 PATCHES=$(ls *.patch | grep -v "^POPULATED")
+PATCHES_NO_COVER=$(ls *.patch | grep -v "^POPULATED" | grep -v 0000)
 RECIPIENTS_FILE=$1
+GENERATE_RECIPIENTS=false
 
-for CURRENT_PATCH in ${PATCHES}; do
-	"${PROGRAM_DIRECTORY}"/populate_patch_with_recipients.sh \
-		-p ${CURRENT_PATCH} \
-		-k \
-		-r ${RECIPIENTS_FILE} \
-		> POPULATED-${CURRENT_PATCH}
-done
+if [ -z "${RECIPIENTS_FILE}" ]; then
+	RECIPIENTS_FILE=$(mktemp)
+	GENERATE_RECIPIENTS=true
+fi
+
+if ${GENERATE_RECIPIENTS}; then
+	for CURRENT_PATCH in ${PATCHES}; do
+		if [[ ${CURRENT_PATCH} == *0000* ]]; then
+			"${PROGRAM_DIRECTORY}"/get_recipients.sh ${PATCHES_NO_COVER} > ${RECIPIENTS_FILE}
+		else
+			"${PROGRAM_DIRECTORY}"/get_recipients.sh ${CURRENT_PATCH} > ${RECIPIENTS_FILE}
+		fi
+		"${PROGRAM_DIRECTORY}"/populate_patch_with_recipients.sh \
+			-p ${CURRENT_PATCH} \
+			-k \
+			-r ${RECIPIENTS_FILE} \
+			> POPULATED-${CURRENT_PATCH}
+	done
+	rm -f ${RECIPIENTS_FILE}
+else
+	for CURRENT_PATCH in ${PATCHES}; do
+		"${PROGRAM_DIRECTORY}"/populate_patch_with_recipients.sh \
+			-p ${CURRENT_PATCH} \
+			-k \
+			-r ${RECIPIENTS_FILE} \
+			> POPULATED-${CURRENT_PATCH}
+	done
+fi
